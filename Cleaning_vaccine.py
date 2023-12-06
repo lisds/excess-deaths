341d43ad217ceaa09901164f794b82891fd055d5
Charlie Pullen <charlie.pullen@lis.ac.uk>
2023-12-06 14:26:27 +0000

    fixing cleaning doc so it runs

--- Cleaning_vaccine.py
+++ Cleaning_vaccine.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-vaccine_types = pd.read_excel('vaccine_types.xlsx')
+vaccine_types = pd.read_excel('data/vaccine_types.xlsx')
 
 #Drop unwanted columns - only focussing on first dose - binary vaccinated or not
 vaccine_types.drop(['Denominator', 'NumberDosesReceived', 'NumberDosesExported', 'FirstDoseRefused', 'SecondDose', 'DoseAdditional1', 'DoseAdditional2', 'DoseAdditional3', 'DoseAdditional4', 'DoseAdditional5', 'UnknownDose'], axis = 1)
@@ -16,7 +16,7 @@ vaccine_types = vaccine_types[vaccine_types['TargetGroup'] == 'ALL']
 vaccine_types = vaccine_types[vaccine_types['ReportingCountry'] == vaccine_types['Region']]
 
 #Drop duplicate rows
-drop_dupes = total_vaccinated.drop_duplicates(subset = ['YearWeekISO', 'ReportingCountry', 'FirstDose'])
+drop_dupes = vaccine_types.drop_duplicates(subset = ['YearWeekISO', 'ReportingCountry', 'FirstDose'])
 
 
 
