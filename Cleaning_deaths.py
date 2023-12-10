9cb77d838ad9011388760797628332d137d0940d
Charlie Pullen <charlie.pullen@lis.ac.uk>
2023-12-10 19:44:57 +0000

    change base name of cleaned deaths with 2023 mean

--- Cleaning_deaths.py
+++ Cleaning_deaths.py
@@ -4,15 +4,15 @@ import numpy as np
 excess_deaths = pd.read_excel('data/excess_death.xlsx', sheet_name = 'Sheet 1', skiprows=7, skipfooter=6)
 
 # Remove columns labelling rates that are estimated or provisional data ** RETURN TO THIS
-justnumbs = excess_deaths.loc[:, ~excess_deaths.columns.str.startswith('Unnamed')][1:]
+excess_deaths_values = excess_deaths.loc[:, ~excess_deaths.columns.str.startswith('Unnamed')][1:]
 
 # Make index country names
-justnumbs = justnumbs.set_index('TIME')
+excess_deaths_values = excess_deaths_values.set_index('TIME')
 
 # Add a mean excess death by EU country for 2023 
-justnumbs = justnumbs.apply(pd.to_numeric, errors='coerce')
-justnumbs['Mean 2023'] = justnumbs.iloc[:, -9:].mean(axis = 1)
-justnumbs.sort_values('Mean 2023')
+excess_deaths_values = excess_deaths_values.apply(pd.to_numeric, errors='coerce')
+excess_deaths_values['Mean 2023'] = excess_deaths_values.iloc[:, -9:].mean(axis = 1)
+excess_deaths_values.sort_values('Mean 2023')
 
 
-print(justnumbs)
+print(excess_deaths_values)
