a2b6e533f11ce5feb6fa34b78d5395c313a2f9fd
Charlie Pullen <charlie.pullen@lis.ac.uk>
2023-12-10 16:37:50 +0000

    Removing estimated/provisional labels  and adding mean column

---
+++ Cleaning_deaths.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+excess_death = pd.read_excel('data/excess_death.xlsx')
+
+# Remove columns labelling rates that are estimated or provisional data ** RETURN TO THIS
+justnumbs = excess_deaths.loc[:, ~excess_deaths.columns.str.startswith('Unnamed')][1:]
+
+# Make index country names
+justnumbs = justnumbs.set_index('TIME')
+
+# Add a mean excess death by EU country for 2023 
+justnumbs = justnumbs.apply(pd.to_numeric, errors='coerce')
+justnumbs['Mean 2023'] = justnumbs.iloc[:, -9:].mean(axis = 1)
+justnumbs.sort_values('Mean 2023')
