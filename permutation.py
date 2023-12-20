f25205ec36e7a0974a18031b9de89f965f08cec7
Daniel Konioukhov <dkoniouk@gmail.com>
2023-12-20 03:49:18 +0000

    Edited write-up from residuals to permutation, edited py files merge, permutation, regress,plot1

--- permutation.py
+++ permutation.py
@@ -34,24 +34,32 @@ import scipy.stats as sps
 
 
 
-
+#making a function that will permute the slope differences
 def permute(Vax1_str, Vax2_str, dict):
+    #creating a random number generator
     rng = np.random.default_rng()
+    #doing the linear regression and then taking the difference of real slopes
     Vax1 = sps.linregress(dict[Vax1_str]['% vaxed'], dict[Vax1_str]['Mean 2023'])
     Vax2 = sps.linregress(dict[Vax2_str]['% vaxed'], dict[Vax2_str]['Mean 2023'])
     real_diff = Vax1.slope - Vax2.slope
+
+    #since only one of the two columns need to be shuffled I will stack the %vaxed
     first_array = dict[Vax1_str]['% vaxed']
     second_array = dict[Vax2_str]['% vaxed']
     pool = np.concatenate([first_array, second_array])
 
-    
+    #this is where my fake differences will be stored
     fake_difference = np.zeros(10000)
     
+    #loop the loop a thousand times
     for i in np.arange(10000):
+        #this will shuffle randomly
         shuff = rng.permutation(pool)
+        #taking the linear regressions of the shuffled values and finding slope difference
         fakeVax1 = sps.linregress(shuff[0:len(first_array)], dict[Vax1_str]['Mean 2023'])
         fakeVax2 = sps.linregress(shuff[len(first_array):], dict[Vax2_str]['Mean 2023'])
         fake_diff = fakeVax1.slope - fakeVax2.slope
         fake_difference[i] = fake_diff
+        #counting how many times the real is found among the fakes
         diff_act = np.count_nonzero(fake_difference <= real_diff)
     return plt.hist(fake_difference) and diff_act;
--- plot1.py
+++ plot1.py
@@ -7,15 +7,15 @@ def plot_vax(vaccine_dict, vax, mean):
     #create 3x6 row of empty subplots
     fig, axes = plt.subplots(nrows=3, ncols=6, figsize=(18, 9))
 
-    #
+    #flattening the 3x6 grid into a one dimensional array
     axes = axes.flatten()
 
-   
+    #iterating over each vaccine key in the dictionary and pulling out its dataset
     for i, (vaccine, table) in enumerate(vaccine_dict.items()):
-        
+
+    #making the scatterplot and naming the labels and title
+
         ax = axes[i]
-
-
         ax.scatter(table[vax], table[mean])
 
         
